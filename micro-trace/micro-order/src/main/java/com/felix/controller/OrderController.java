a2860436f93374c51b256fa75aeb57e5e8945d36
赵立山 <zhaolishan@aipark.com>
2023-09-21 11:09:39 +0800

    日志输出配置调整

--- micro-trace/micro-order/src/main/java/com/felix/controller/OrderController.java
+++ micro-trace/micro-order/src/main/java/com/felix/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.felix.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     @GetMapping("/get")
-    public Object get(@RequestParam("id") String id){
+    public Object get(@RequestParam("orderId") String orderId){
+
+        log.trace("====> /order/get trace, orderId={}", orderId);
+        log.debug("====> /order/get debug, orderId={}", orderId);
+        log.info("====> /order/get info, orderId={}", orderId);
+        log.warn("====> /order/get warn, orderId={}", orderId);
+        log.error("====> /order/get error, orderId={}", orderId);
+
         ObjectMapper objectMapper = new ObjectMapper();
         ObjectNode objectNode = objectMapper.createObjectNode();
-        return objectNode.put("id", id);
+        return objectNode.put("orderId", orderId);
     }
 }
