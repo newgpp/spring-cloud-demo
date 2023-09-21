74a89e7b20800810319c5c084083213e2a360254
赵立山 <zhaolishan@aipark.com>
2023-09-21 15:17:40 +0800

    获取traceId示例

--- README.md
+++ README.md
@@ -5,8 +5,41 @@
 - micro-eureka
 - micro-config-server
 
+### micro-eureka
+```html
+https://cloud.spring.io/spring-cloud-netflix/reference/html/
+```
+
 ### micro-config-server
+```html
+https://docs.spring.io/spring-cloud-config/docs/current/reference/html
+```
 
 ```bash
+# 拉取配置文件
 curl http://127.0.0.1:8762/common/template/master
+```
+
+### micro-trace
+
+```bash
+# feign & traceId
+curl 'http://127.0.0.1:8763/order/get?orderId=11111'
+
+# get traceId
+curl http://localhost:8763/order/traceId
+```
+
+- Spring Cloud OpenFeign
+```html
+https://cloud.spring.io/spring-cloud-openfeign/reference/html/
+
+https://www.baeldung.com/spring-cloud-openfeign
+```
+- Spring Cloud Sleuth
+```html
+https://docs.spring.io/spring-cloud-sleuth/docs/2.2.8.RELEASE/reference/html/
+
+https://www.baeldung.com/spring-cloud-sleuth-single-application
+https://www.baeldung.com/spring-cloud-sleuth-get-trace-id
 ```
--- micro-trace/micro-order/src/main/java/com/felix/controller/OrderController.java
+++ micro-trace/micro-order/src/main/java/com/felix/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.felix.controller;
 
+import brave.Span;
+import brave.Tracer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.felix.feign.ExpressFeignClient;
@@ -24,20 +26,34 @@ public class OrderController {
 
     @Resource
     private ExpressFeignClient expressFeignClient;
+    @Resource
+    private Tracer tracer;
 
     @GetMapping("/get")
-    public Object get(@RequestParam("orderId") String orderId){
-
+    public Object getOrderExpress(@RequestParam("orderId") String orderId) {
         log.trace("====> /order/get trace, orderId={}", orderId);
         log.debug("====> /order/get debug, orderId={}", orderId);
         log.info("====> /order/get info, orderId={}", orderId);
         log.warn("====> /order/get warn, orderId={}", orderId);
         log.error("====> /order/get error, orderId={}", orderId);
-
         Object orderExpress = expressFeignClient.get(orderId);
-
         ObjectMapper objectMapper = new ObjectMapper();
         ObjectNode objectNode = objectMapper.createObjectNode();
         return objectNode.put("orderId", orderId).putPOJO("express", orderExpress);
+    }
+
+    @GetMapping("/traceId")
+    public Object getSleuthTraceId() {
+        Span span = tracer.currentSpan();
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        if (span != null) {
+            String traceId = span.context().traceIdString();
+            String spanId = span.context().spanIdString();
+            log.info("Trace ID {}", traceId);
+            log.info("Span ID {}", spanId);
+            return objectNode.put("traceId", traceId).put("spanId", spanId);
+        }
+        return objectNode;
     }
 }
