efd2952d0489831a240f706876d17c3a26fef0e8
赵立山 <zhaolishan@aipark.com>
2023-09-21 14:19:00 +0800

    feign熔断配置

--- micro-trace/micro-order/src/main/java/com/felix/feign/ExpressFeignClient.java
+++ micro-trace/micro-order/src/main/java/com/felix/feign/ExpressFeignClient.java
@@ -1,5 +1,6 @@
 package com.felix.feign;
 
+import com.felix.feign.fallback.ExpressFeignFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author felix
  * @desc some desc
  */
-@FeignClient(name = "micro-express")
+@FeignClient(name = "micro-express", fallbackFactory = ExpressFeignFallbackFactory.class)
 public interface ExpressFeignClient {
 
     @GetMapping("/express/get")
--- micro-trace/micro-order/src/main/java/com/felix/feign/fallback/ExpressFeignFallbackFactory.java
+++ micro-trace/micro-order/src/main/java/com/felix/feign/fallback/ExpressFeignFallbackFactory.java
@@ -1,30 +1,31 @@
-//package com.felix.feign.fallback;
-//
-//import com.felix.feign.ExpressFeignClient;
-//import feign.hystrix.FallbackFactory;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @author admin
-// */
-//@Component
-//public class ExpressFeignFallbackFactory implements FallbackFactory<ExpressFeignClient> {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(ExpressFeignFallbackFactory.class);
-//
-//    @Override
-//    public ExpressFeignClient create(Throwable cause) {
-//
-//        logger.info("ExpressFeignClient error：", cause);
-//
-//        return new ExpressFeignClient() {
-//            @Override
-//            public Object get(String orderId) {
-//                return null;
-//            }
-//        };
-//    }
-//
-//}
+package com.felix.feign.fallback;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.felix.feign.ExpressFeignClient;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author admin
+ */
+@Component
+public class ExpressFeignFallbackFactory implements FallbackFactory<ExpressFeignClient> {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExpressFeignFallbackFactory.class);
+
+    @Override
+    public ExpressFeignClient create(Throwable cause) {
+
+        logger.error("ExpressFeignClientFallback：", cause);
+
+        return new ExpressFeignClient() {
+            @Override
+            public Object get(String orderId) {
+                return new ObjectMapper().createObjectNode().put("errMsg", cause.getMessage());
+            }
+        };
+    }
+
+}
