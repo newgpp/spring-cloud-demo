f3a7e971303ed02d360d18c335ff438ef1c4afe5
赵立山 <zhaolishan@aipark.com>
2023-10-20 16:36:19 +0800

    redis run lua demo

--- micro-data/micro-mysql/src/test/java/com/felix/utils/IdUtilTest.java
+++ micro-data/micro-mysql/src/test/java/com/felix/utils/IdUtilTest.java
@@ -2,6 +2,8 @@ package com.felix.utils;
 
 import org.junit.Test;
 
+import java.util.regex.Pattern;
+
 /**
  * @author felix
  * @desc some desc
@@ -15,6 +17,19 @@ public class IdUtilTest {
             long id = IdUtil.nextSnowflakeId();
             System.out.println(id + "  " + System.currentTimeMillis());
         }
+    }
+    
+    @Test
+    public void test_02(){
+        Pattern datePattern = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
+        String date01 = "1";
+        System.out.println("日期: " + date01 + "  校验通过: " + datePattern.matcher(date01).matches());
+
+        String date02 = "2023-10-19";
+        System.out.println("日期: " + date02 + "  校验通过: " + datePattern.matcher(date02).matches());
+
+
+        System.out.println("2023-10-19".compareTo("2023-10-20"));
     }
 
 }
