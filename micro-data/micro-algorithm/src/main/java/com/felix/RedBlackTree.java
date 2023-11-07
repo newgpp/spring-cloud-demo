ccfecc9afa89005882e2818e54b4fcaefd5da424
赵立山 <zhaolishan@aipark.com>
2023-11-07 09:13:55 +0800

    red black tree

---
+++ micro-data/micro-algorithm/src/main/java/com/felix/RedBlackTree.java
@@ -0,0 +1,19 @@
+package com.felix;
+
+/**
+ * @author felix
+ * @desc some desc
+ * https://www.cnblogs.com/crazymakercircle/p/16320430.html#autoid-h3-5-0-0
+ * https://zhuanlan.zhihu.com/p/143585797
+ * https://www.jianshu.com/p/86a1fd2d7406
+ *
+ * 五大特性
+ * 1. 每个节点要么是红的要么是黑的
+ * 2. 跟节点是黑色
+ * 3. 每个叶节点（树尾端NULL节点）都是黑色
+ * 4. 如果一个节点是红色，那么
+ *
+ *
+ */
+public class RedBlackTree {
+}
